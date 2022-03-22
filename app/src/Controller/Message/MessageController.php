2da049c0bd084c50a2fcdcb289dbce122f42cd0e
Azdra <baptiste.brand2002@gmail.com>
2022-03-22 21:17:19 +0100

    Change ChannelService by MessageService

--- app/src/Controller/Message/MessageController.php
+++ app/src/Controller/Message/MessageController.php
@@ -4,7 +4,7 @@ namespace App\Controller\Message;
 
 use App\Controller\DefaultApiController;
 use App\Entity\Channel\Channel;
-use App\Service\Channel\ChannelService;
+use App\Service\Message\MessageService;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,10 +19,10 @@ class MessageController extends DefaultApiController
     /**
      * @Rest\Post("/messages")
      */
-    public function createMessage(Request $request, Channel $channel, ChannelService $channelService): JsonResponse
+    public function createMessage(Request $request, Channel $channel, MessageService $messageService): JsonResponse
     {
         return $this->handleRequest($request, [
-           'service' => $channelService,
+           'service' => $messageService,
            'function' => 'createMessage',
            'args' => [$channel]
         ]);
