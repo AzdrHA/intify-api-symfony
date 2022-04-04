416e929f2d751aea420812e0cde5a7cf7cd86127
Azdra <baptiste.brand2002@gmail.com>
2022-04-04 12:02:17 +0200

    add new bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
